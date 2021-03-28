5a73b2dc4d5931f7685bb8e11f311f30994985c6
BbbondSPb <66222881+BbbondSPb@users.noreply.github.com>
2021-03-28 18:31:50 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
 module.exports = function createDreamTeam(array) {
   str = '';
     for (let i = 0; i < array.length; i++) {
