84ec1df55127a5eb804fe4dc73672eff86f0082e
BbbondSPb <66222881+BbbondSPb@users.noreply.github.com>
2021-03-28 20:00:36 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(array) {
-  let myArray = [];
-  for (let i = 0; i < array.length; i++) {
-      if (array[i] == '--discard-next') {
-         i++;
-      } else if (array[i] == '--discard-prev') {
-          if (myArray.length >= 1) {
-              myArray.pop();
-          }
-      } else if (array[i] == '--double-next') {
-          if (array[i + 1]) {
-              myArray.push(array[i + 1]);
-          }
-      } else if (array[i] == '--double-prev') {
-          if (myArray.length >= 1) {
-              myArray.push(array[i - 1]);
-          }
-      } else {
-          myArray.push(array[i]);
-      }
-  }
-  return myArray
+     if (typeof(array) == 'object') {
+    let myArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == '--discard-next') {
+           i++;
+        } else if (array[i] == '--discard-prev') {
+            if (myArray.length >= 1) {
+                myArray.pop();
+            }
+        } else if (array[i] == '--double-next') {
+            if (array[i + 1]) {
+                myArray.push(array[i + 1]);
+            }
+        } else if (array[i] == '--double-prev') {
+            if (myArray.length >= 1) {
+                myArray.push(array[i - 1]);
+            }
+        } else {
+            myArray.push(array[i]);
+        }
+    }
+    return myArray
+} else {
+    return 'Error'
+}
 };
