438e8b4749af3ac0c9c3d5d91385b66b0f2a2704
BbbondSPb <66222881+BbbondSPb@users.noreply.github.com>
2021-03-30 00:44:37 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
- let result = '';
+  let result = '';
     if (!options.separator) {
     options.separator = '+';
    };
@@ -16,13 +16,16 @@ module.exports = function repeater(str, options) {
    };
    for (let j = options.additionRepeatTimes; j > 0; j--) {
    result = result + str;
-   for (let i = options.additionRepeatTimes; i > 0; i--) {
-       if (i > 1) {
-        result = result + options.addition + options.additionSeparator;
-       } else {
-        result = result + options.addition;
-       }
+   if (options.addition) {
+    for (let i = options.additionRepeatTimes; i > 0; i--) {
+        if (i > 1) {
+         result = result + options.addition + options.additionSeparator;
+        } else {
+         result = result + options.addition;
+        }
+    }
    }
+  
    if (j > 1) {
     result = result + options.separator 
    } 
