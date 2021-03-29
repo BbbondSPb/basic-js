86aa962d474989b76c85708fa11b4f0261737eaf
BbbondSPb <66222881+BbbondSPb@users.noreply.github.com>
2021-03-29 15:44:02 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+ if (date.getMonth() == 11 || date.getMonth() == 00 || date.getMonth() == 01) {
+    return 'winter'
+  } else  if (date.getMonth() == 02 || date.getMonth() == 03 || date.getMonth() == 04) {
+    return 'spring'
+  } else  if (date.getMonth() == 05 || date.getMonth() == 06 || date.getMonth() == 07) {
+    return 'summer'
+  } else  if (date.getMonth() == 08 || date.getMonth() == 09 || date.getMonth() == 10) {
+    return 'autumn'
+  }
 };
