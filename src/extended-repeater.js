1b590f9c14640e1db601f6929632302a391863b9
BbbondSPb <66222881+BbbondSPb@users.noreply.github.com>
2021-03-30 00:37:19 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+ let result = '';
+    if (!options.separator) {
+    options.separator = '+';
+   };
+   if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+   };
+   if (!options.additionRepeatTimes) {
+    return ' '
+   }; 
+   if (!options.repeatTimes) {
+    return ' '
+   };
+   for (let j = options.additionRepeatTimes; j > 0; j--) {
+   result = result + str;
+   for (let i = options.additionRepeatTimes; i > 0; i--) {
+       if (i > 1) {
+        result = result + options.addition + options.additionSeparator;
+       } else {
+        result = result + options.addition;
+       }
+   }
+   if (j > 1) {
+    result = result + options.separator 
+   } 
+ }
+   return result
 };
   
